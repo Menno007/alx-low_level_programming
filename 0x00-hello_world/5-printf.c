b7931d839014f1a773d44362fc545020a7ac4f81
Menno007 <Menna.soft3@gmail.com>
2023-09-07 06:45:13 -0700

    let's print again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - printf "with proper grammar, but the outcome is a piece of art,"
- * 
+ * main - printf "with proper grammar, but the outcome is a piece of art," 
  * Return: Always 0
 */
 int main(void)
