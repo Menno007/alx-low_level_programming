2bf31700cab269d1d5a3fdf5492802e07608c87f
Menno007 <Menna.soft3@gmail.com>
2023-09-08 18:54:54 -0700

    print alphapitic

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ **/
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	if(l != q || l != e)
+	{
+		putchar(l);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
