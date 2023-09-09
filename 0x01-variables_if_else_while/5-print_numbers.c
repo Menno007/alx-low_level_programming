7caa2611f9554270846755f76fbabc90ba36083e
Menno007 <Menna.soft3@gmail.com>
2023-09-08 19:07:35 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ **/
+int main(void)
+{
+        int n;
+
+        for (n = 0; n < 10; n++)
+                printf("%d", n);
+
+        return (0);
+}
