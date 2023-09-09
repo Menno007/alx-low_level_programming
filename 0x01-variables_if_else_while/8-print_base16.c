8d95a91e60ec0d054d1cd83a80b7705ea4f9816d
Menno007 <Menna.soft3@gmail.com>
2023-09-09 03:38:47 -0700

    print hexa16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,13 @@
 int main(void)
 {
 	int x;
-
-	for (x = 0; x < 16; x++)
+	char y;
+	for (x = 0; x < 10; x++)
 	{
 		putchar(x + '0');
 	}
+	for (y = 'a'; y <= 'f'; y++)
+		putchar (y);
 	putchar('\n');
 
 	return (0);
