f2dc47192ed93132c2124bf93231d2e0b4a725d2
Menno007 <Menna.soft3@gmail.com>
2023-09-09 04:31:10 -0700

    print number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
  * main - main block print numbers
- * Describtion: using putchar to print numbers
+ * Description: using putchar to print numbers.
  * Return: Always 0.
 */
 int main(void)
