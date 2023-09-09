b82ee4bcf6666b8e56fa42466e4d0dd8da8041f7
Menno007 <Menna.soft3@gmail.com>
2023-09-08 20:16:39 -0700

    print hexa16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all the numbers of base 16 in lowercase.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 16; x++)
+	{
+        	putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
