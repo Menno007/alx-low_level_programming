32b8f3191a762c5926695116fe2a622babb74bd9
Menno007 <Menna.soft3@gmail.com>
2023-09-08 18:33:57 -0700

    print alphapitic

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ **/
+int main(void)
+{
+        char l;
+
+        for (l = 'a'; l <= 'z'; l++)
+                putchar(l);
+
+        putchar('\n');
+	for (l = 'A'; l <= 'Z'; l++)
+                putchar(l);
+        return (0);
+}
