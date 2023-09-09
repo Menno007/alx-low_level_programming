1333568839605e0d9a0eebae89a5c3cee39b18ab
Menno007 <Menna.soft3@gmail.com>
2023-09-08 19:52:02 -0700

    print reverse alpha

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,12 @@
  **/
 int main(void)
 {
-        char l;
+	char l;
 
-        for (l = 'z'; l >= 'a'; l--)
-                putchar(l);
+	for (l = 'z'; l >= 'a'; l--)
+		putchar(l);
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
