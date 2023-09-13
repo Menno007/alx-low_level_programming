b612b5702ec2930a64617d4957f65aa9b5d93f02
Menno007 <Menna.soft3@gmail.com>
2023-09-13 15:21:16 -0700

    checking uppercase character

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - writes the character c to stdout
+ * @c: The number to print it's sign
+ * Return: Always 0.
+ */
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
+	}
 	if (n = 0)
+	{
 		_putchar('0');
 		return (0);
+	}
 	if (n < 0)
+	{
 		_putchar('-');
 		return (-1);
+	{
 }
