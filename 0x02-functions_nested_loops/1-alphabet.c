a2d63f4993a8f40c198f7ddbab71fbc78a617de2
Menno007 <Menna.soft3@gmail.com>
2023-09-13 12:36:36 -0700

    print alphapet small putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ **/
+void print_alphabet(void)
+{
+	int i
+	
+	for(i=27; i<122; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+
+	return (0);
+}
