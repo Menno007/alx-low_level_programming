8256bf0438b86fd03fa762eb96c8030d349d92c9
Menno007 <Menna.soft3@gmail.com>
2023-09-13 15:46:33 -0700

    the absloute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _abs - writes the the absolute value of an integer.
- * @c: The number
+ * @n: The number
  *
  * Return: Always 0.
  */
 int _abs(int n)
 {
 	if (n < 0)
-		n= -1 * n;
+		n = -1 * n;
 	return (n);
 }
