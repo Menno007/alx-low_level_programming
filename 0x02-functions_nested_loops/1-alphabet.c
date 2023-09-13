810eff15903cacc35ad22e12ea995d31e98181ea
Menno007 <Menna.soft3@gmail.com>
2023-09-13 13:07:46 -0700

    print alphapet small putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * print_alphabet - fucntion prints the small alphabet from a to z
  *
- *
- **/
+ * Return: Always 0.
+ */
 void print_alphabet(void)
 {
 	int i;
