b39865c0ae63e1d64ed2fd0c7ad3720238de986e
Menno007 <Menna.soft3@gmail.com>
2023-09-13 13:21:18 -0700

    print alphapet small putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include main.h
+/**
+ * print_alphabet - function prints alphabet 10 times
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int n;
+
+	for (n = 0; n < 10;n++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		 _putchar('\n');
+	}
+	_putchar('\n');
+}
