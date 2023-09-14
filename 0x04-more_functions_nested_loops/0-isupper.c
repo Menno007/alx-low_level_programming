a5455ea40baab4ef7258aa9339742b2d6f091f15
Menno007 <Menna.soft3@gmail.com>
2023-09-14 13:56:46 -0700

    print compare if character is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isupper- function compare lower
+ *@c: is character checker
+ *Return: Always 0.
+ */
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 69)
+		return (1);
+
+	return (0);
+
+}
