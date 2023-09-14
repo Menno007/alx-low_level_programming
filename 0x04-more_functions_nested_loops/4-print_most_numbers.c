a6318601b9aca778ba1a4f647661fab24e4f336c
Menno007 <Menna.soft3@gmail.com>
2023-09-14 15:32:45 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - blablabla
+ *
+ * Description: blablabla
+ *
+ * Return: blablabla
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		if ((x != 52) && (x != 50))
+		_putchar(x);
+	}
+	_putchar('\n');
+}
