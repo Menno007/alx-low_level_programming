db77aa55241b49c7015e348957c6bf1b830c7ddd
Menno007 <Menna.soft3@gmail.com>
2023-09-14 12:25:31 -0700

    hour:min

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - his fucntion prints the alphabet from a to z
+ *
+ * Description: this fucntion prints the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	for(x=00;x<=24;x++)
+	{
+		for(y=00;y<=59;y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		
+		}
+	}
+}
