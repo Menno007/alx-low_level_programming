6fab1d358190ed0a713a73f285d5a03018af3e19
Menno007 <Menna.soft3@gmail.com>
2023-09-14 12:36:03 -0700

    hour:min

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,11 +8,11 @@
  */
 void jack_bauer(void)
 {
-	int x, y ;
+	int x, y;
 
-	for(x=00;x<=23;x++)
+	for(x = 00; x <= 23; x++)
 	{
-		for(y=00;y<=59;y++)
+		for(y = 00; y <= 59; y++)
 		{
 			_putchar((x / 10) + '0');
 			_putchar((x % 10) + '0');
