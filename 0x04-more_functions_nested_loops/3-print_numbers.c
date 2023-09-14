0172ee100c8e4829de43be2091f5a6b58256f3d1
Menno007 <Menna.soft3@gmail.com>
2023-09-14 15:03:59 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - blablabla
+ *
+ * Description: blablabla
+ *
+ * Return: blablabla
+ */
+void print_numbers(void)
+{
+	int x;
+	for (x = 48; x <= 59; x++)
+	{
+		_putchar('x');
+		_putchar('\n');
+	}
+}
