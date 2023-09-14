bf3242fa47b89934bcc508bbacfd82c37f02d13a
Menno007 <Menna.soft3@gmail.com>
2023-09-14 08:39:24 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,14 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-	
+
 	if (n < 0)
 		last_digit = ((n % 10) * (-1));
-	
+
 	else
 		last_digit = n % 10;
 
 	_putchar('0' + last_digit);
-	
+
 	return (last_digit);
 }
