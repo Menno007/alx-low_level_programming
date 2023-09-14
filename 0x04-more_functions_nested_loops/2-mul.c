da4b4fe48351c3c664e5447632830d0aa24b644c
Menno007 <Menna.soft3@gmail.com>
2023-09-14 14:41:31 -0700

    sum 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - blablabla
+ * @b: blablabla
+ * @a: blablabla
+ * Description: blablabla
+ * Return: blablabla
+ */
+int mul(int a, int b)
+{
+	int sum;
+	sum = a * b;
+
+	return (sum);
+}
