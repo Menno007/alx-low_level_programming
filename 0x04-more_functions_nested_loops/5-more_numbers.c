5f132557f37481489d94c831e1a6d3de716e111e
Menno007 <Menna.soft3@gmail.com>
2023-09-14 16:20:38 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - blablabla
+ *
+ * Description: blablabla
+ *
+ * Return: blablabla
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x <= 14 ; x++)
+		{
+			if (x >= 10)
+				_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');	
+		}
+		_putchar('\n');
+	}
+}
