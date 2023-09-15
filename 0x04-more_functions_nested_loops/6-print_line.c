5abae27b597b133ff718931b320120b415b5ff34
Menno007 <Menna.soft3@gmail.com>
2023-09-14 17:03:45 -0700

    print _

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,15 @@
 #include "main.h"
 /**
- * void print_line - blablabla
- *
+ * print_line - blablabla
+ * @n: blabla
  * Description: blablabla
- *
  * Return: blablabla
  */
 void print_line(int n)
 {
 	int x;
-	
-	if ((n == 0) || (n < 0))
-		_putchar('\n');
 
 	for (x = 0; x <= n; x++)
 		_putchar('_');
 	_putchar('\n');
-
 }
