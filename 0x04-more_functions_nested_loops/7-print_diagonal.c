f6b97ab26ceaf2548d8b72eb58d1eb0f8ea5673e
Menno007 <Menna.soft3@gmail.com>
2023-09-15 05:13:18 -0700

    print diagonal with nested loop

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - blablabla
+ * @n: blabla
+ * Description: blablabla
+ * Return: blablabla
+ */
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+
+	if (n > 0)
+	{
+	for (x = 0; x < n; x++)
+	{
+		for (y = 0; y < x; y++)
+		{
+			_putchar(' ');
+		}
+	_putchar('\');
+	}
+	}
+	_putchar('\n');
+}
