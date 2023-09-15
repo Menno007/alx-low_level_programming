9b0b134ac435c6ee040fe6df9060ad45d2d2ea12
Menno007 <Menna.soft3@gmail.com>
2023-09-15 13:32:28 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void main(void)
+{
+	int i, x;
+	
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 )
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+			printf("%d ", i);
+
+	}
+	printf("\n");
+}
