47116a446b54e0d2d04031ee1bd22f1dbc7cce50
Menno007 <Menna.soft3@gmail.com>
2023-09-15 08:19:07 -0700

    nested loop

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - blablabla
+ * @size: blablabla
+ * Description: blablabla
+ * Return: blablabla
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y=0; y < size; y++)
+	{
+			putchar('#');
+		}
+	putchar('\n');
+	}
+}
