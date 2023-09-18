a8afdd5246c57a590c1cb102a6622469d7efe0e0
Menno007 <Menna.soft3@gmail.com>
2023-09-18 16:04:19 -0700

    pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 include "main.h"
 #include <stdio.h>
 /**
- *
- *
- **/
+ * reset_to_98 - takes a pointer as a parameter and change its value
+ * @n: parameter
+ * Return: 0
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
