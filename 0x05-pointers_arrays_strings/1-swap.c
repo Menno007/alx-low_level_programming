198b07952afeba4fcc5c943492ce3055439f0ddc
Menno007 <Menna.soft3@gmail.com>
2023-09-18 17:05:09 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ * swap_int - function to swap 2 values
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 0 (Success);
+ */
+void swap_int(int *a, int *b)
+{
+	int swap1, swap2;
+
+	swap1 = *a;
+	swap2 = *b;
+	*b = swap1;
+	*a = swap2;
+}
