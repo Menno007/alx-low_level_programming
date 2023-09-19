62c5765b49b8af8b1107b2152722b06b2a19679d
Menno007 <Menna.soft3@gmail.com>
2023-09-19 10:37:18 -0700

    revstring

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i, j;
+	for (; s[len] != '\0';)
+	{
+		len++;
+	}
+	len--;
+	for (i = len,j = 0;i > len/2 || j < len/2 ; i--,j++)
+	{
+		swap = s[len];
+		s[len] = s[j];
+		s[j] = swap;
+	}
+}
