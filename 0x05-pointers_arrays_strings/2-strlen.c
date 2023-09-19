392e8a9f46b18650dfb52e97830735ebd10af035
Menno007 <Menna.soft3@gmail.com>
2023-09-18 17:47:43 -0700

    pointer

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * swap_int - function to swap 2 values
- * @a: first parameter
- * @b: second parameter
+ * @s: first parameter
  * Return: 0 (Success);
  */
 int _strlen(char *s)
 {
-	int sum = 1;
+	int sum = 0;
 
 	for(; *s != '\0'; s++)
 	{
