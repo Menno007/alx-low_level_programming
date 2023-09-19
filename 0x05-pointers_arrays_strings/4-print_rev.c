9167d95099314694c05e7606561634ad11bd0f8b
Menno007 <Menna.soft3@gmail.com>
2023-09-18 19:21:56 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - function to swap 2 values
+ * @s: first parameter
+ * Return: 0 (Success);
+ */
+void print_rev(char *s)
+{
+	int len=0;
+
+	for (; *s != '\0'; s++)
+	{
+		len++;
+	}
+	for (; len > 0; len--,s--)
+	{
+		putchar(*s);
+	}
+	putchar('\n');
+}
