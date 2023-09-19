7f156728d3ee6574e0f92ba7bcfeabff0cd56fb8
Menno007 <Menna.soft3@gmail.com>
2023-09-19 16:05:29 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - function
+ * @str: first parameter
+ * Return: 0 (Success);
+ */
+void puts2(char *str)
+{
+	int len=0;
+
+	for ( ;s[len] != '\0' ; len = len + 2 /* len +=2 */ )
+	{
+		printf("%c \n", s[len]);
+	}
+}
