25b982b1e32152bfabd7b2a18ba3548ae19569a8
Menno007 <Menna.soft3@gmail.com>
2023-09-20 16:26:28 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	src[j] = '\0';
+	}
+	return (dest);
+}
