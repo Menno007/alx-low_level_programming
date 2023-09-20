76192a27384365c1908c8e6b8858e8bc0d46f546
Menno007 <Menna.soft3@gmail.com>
2023-09-20 14:10:37 -0700

    copy string array

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,14 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	return dest;
+	if(src[i] == '\0')
+		dest[i] = src[i];
+
+	return (dest);
 }
