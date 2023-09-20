87677ae3e1036685c5076a2a16c5d1a050f74c8d
Menno007 <Menna.soft3@gmail.com>
2023-09-20 16:12:32 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,17 +11,18 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
+
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 		;
 	}
 	if (dest[i] == '\0')
 	{
-		for (j = 0; src[j] != '\0'; j++,i++)
+		for (j = 0; src[j] != '\0'; j++, i++)
 		{
 			dest[i] = src[j];
 		}
-		src [j] = '\0';
+		src[j] = '\0';
 	}
 	return (dest);
 }
