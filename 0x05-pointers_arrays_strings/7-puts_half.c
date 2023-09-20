9725c30466d95f6d42116a5d2b9aafa7a55e6ed5
Menno007 <Menna.soft3@gmail.com>
2023-09-19 17:26:38 -0700

    print last half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - function
+ * @str: first parameter
+ * Return: 0 (Success);
+ */
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+	int i;
+
+	for (i = len / 2; i <= len ;i++)
+	{
+		_putchar(str[i]);
+
+	}
+	_putchar('\n');
+}
+/**
+ * _strlen - function to swap 2 values
+ * @s: first parameter
+ * Return: 0 (Success);
+ */
+int _strlen(char *s)
+{
+	int sum = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		sum++;
+	}
+
+	return (sum);
+}
