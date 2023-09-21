28c833034325d4d35581cec06590dba2060715bf
Menno007 <Menna.soft3@gmail.com>
2023-09-21 12:17:17 -0700

    copy 2 arrays

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0, j = 0; dest[i] != '\0' && j < n; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+}
