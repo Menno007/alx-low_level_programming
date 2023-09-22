55dbb1a1ac4466d1747d466d15be3a6344b56d95
Menno007 <Menna.soft3@gmail.com>
2023-09-21 18:19:35 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,16 +12,16 @@ char *cap_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		{
 			s[i] -= 32;
-
-			for (j = 0; j < 13; j++)
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
 			{
-				if (s[i] == spe[j])
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
-					if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					{
-						s[i + 1] -= 32;
-					}
+					s[i + 1] -= 32;
 				}
 			}
 	}
