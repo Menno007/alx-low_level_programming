e281f374aacc0943a6cc024287a94cbd8f2616a0
Menno007 <Menna.soft3@gmail.com>
2023-09-25 15:56:38 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fill a block of memory
+ * @s: starting address of memory
+ * @b: the desired value
+ * @n: member of bytes to be changed
+ *
+ * Return: Always value
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (unsigned int i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return s;
+}
