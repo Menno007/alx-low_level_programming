b1c5171db197bf4f3afc412368980a45ccfe7fc4
Menno007 <Menna.soft3@gmail.com>
2023-09-26 15:36:03 -0700

    task3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,13 +9,21 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i;
 	int j;
+	int n = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] != ' ')
+		{	
 		for (j = 0; accept[j] != '\0'; j++)
-			if (s[i] == s [j])
-				return (i);
+			if (s[i] == accept[j])
+			{
+				n++;
+			}
+		}
+		else
+			return(n);
 	}
 
-	return (0);
+	return (n);
 }
