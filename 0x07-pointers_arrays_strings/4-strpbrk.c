107a5d5652cd3c8b563a13395213b147ede82b1f
Menno007 <Menna.soft3@gmail.com>
2023-09-26 16:16:56 -0700

    task4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,18 +11,14 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 	int j;
-	int n = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] != ' ')
-		{
 		for (j = 0; accept[j] != '\0'; j++)
 			if (s[i] == accept[j])
 			{
 				return (s + i);
 			}
-		}
 	}
 
 	return (0);
