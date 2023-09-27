f3bacb5fd169f84b48860406300898b9d2170985
Menno007 <Menna.soft3@gmail.com>
2023-09-26 18:04:37 -0700

    task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr - first occurrence of the substring needle in the string haystack
+ * @haystack: main str to be examined
+ * @needle: searched in hystack
+ * Return: return 0
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == *needle)
+		for (j = 0; needle[j] != '\0'; j++)
+			if (haystack[i + j] == needle[j])
+			{
+				return (haystack + i);
+				break;
+			}
+	}
+
+	return (NULL);
+}
