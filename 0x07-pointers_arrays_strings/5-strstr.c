391caf0f9506f8798d25f367c7b5e20e6e95b902
Menno007 <Menna.soft3@gmail.com>
2023-09-27 12:17:58 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,6 +13,10 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		if (haystack[i] == *needle)
@@ -22,7 +26,7 @@ char *_strstr(char *haystack, char *needle)
 				if (haystack[i + j] != needle[j])
                 		{
                         		break;
-                		}	
+                		}
 			}
 			if (needle[j] != '\0')
 				i++;
