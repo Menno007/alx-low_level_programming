1e913c87c294c9faf755c6764cf1ad2c0471f74d
Menno007 <Menna.soft3@gmail.com>
2023-09-27 17:56:51 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints og two diagonal of a square
+ * @a: pointer to array
+ * @size: size of array
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum = sum + a[(size * i) + 1];
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 = sum2 + a[(size * i) + (size - i - 1)];
+	}
+	printf("%d, %d\n", sum, sum2);
+}
