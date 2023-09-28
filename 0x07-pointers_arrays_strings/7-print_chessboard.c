c8126aa19c6f8a75212842a2ca7e352d12574cd7
Menno007 <Menna.soft3@gmail.com>
2023-09-27 17:16:50 -0700

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
 void print_chessboard(char (*a)[8])
 {
 	int i;
 	int x;
 
-	for (i = 0; i < 8;i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (x = 0; x < 8; x++)
 		{
