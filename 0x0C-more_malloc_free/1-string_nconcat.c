4a70b181f1abbdb0b451b03155d4f65676b8009c
Menno007 <Menna.soft3@gmail.com>
2023-10-07 18:36:27 +0300

    edit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,34 +11,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *string;
-    unsigned long int i = 0;
-	unsigned long int j = 0;
-    unsigned long int x = strlen(s1);
+char *string;
+unsigned long int i = 0;
+unsigned long int j = 0;
+unsigned long int x = strlen(s1);
 
-    if (s1 == NULL)
-    {
-    s1 = "";
-    }
-    if (s2 == NULL)
-    {
-    s2 = "";
-    }
-    string = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
-    if (string == NULL)
-    {
-    return (NULL);
-    }
-    for (i = 0; i < strlen(s1); i++)
-		string[i] = s1[i];
-	for (j = 0; j < (n + strlen(s2)); j++, i++)
-		string[i] = s2[j];
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+string = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+if (string == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < strlen(s1); i++)
+string[i] = s1[i];
+for (j = 0; j < (n + strlen(s2)); j++, i++)
+string[i] = s2[j];
 if (n >= strlen(s2))
 {
 string[strlen(s1) + strlen(s2)] = '\0';
 }
 else
 string[x + n] = '\0';
-	return (string);
+return (string);
 
 }
