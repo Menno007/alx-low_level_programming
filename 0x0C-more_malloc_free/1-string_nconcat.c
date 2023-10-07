925d7c185e1222bf6f17ac14c0024ee22171f644
Menno007 <Menna.soft3@gmail.com>
2023-10-07 20:41:07 +0300

    m

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,9 +16,6 @@ unsigned long int i = 0;
 unsigned long int j = 0;
 unsigned long int len1, len2, total_len;
 
-len1 = strlen(s1);
-len2 = strlen(s2);
-
 if (s1 == NULL)
 {
 s1 = "";
@@ -27,6 +24,10 @@ if (s2 == NULL)
 {
 s2 = "";
 }
+
+len1 = strlen(s1);
+len2 = strlen(s2);
+
 if (n >= strlen(s2))
 {
 total_len = len1 + len2;
