bd19c50387aecad5e54e2e7aa78470f3c3c16f21
Menno007 <Menna.soft3@gmail.com>
2023-10-07 07:27:42 +0300

    test

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,10 +9,9 @@
 void *malloc_checked(unsigned int b)
 {
 void *pointer;
-pointer = malloc(b * 4);
+
+pointer = malloc(b);
 if (pointer == NULL)
-{
-exit(98);
-}
+    exit (98);
 return (pointer);
 }
