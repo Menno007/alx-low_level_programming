46519d79a6800df130124db80321d0152c0e5b3f
Menno007 <Menna.soft3@gmail.com>
2023-10-10 03:52:38 +0300

    short if with 0; at the end is used as a placeholder for the "default"

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,18 +18,23 @@ int main(void)
 	/* your code goes there */
 	lastdigit = n % 10;
 	printf("Last digit of %d is %d", n, lastdigit);
-	if (lastdigit > 5)
-	{
-	printf(" and is greater than 5\n");
-	}
-	else if (lastdigit < 6 && lastdigit != 0)
-	{
-	printf(" and is less than 6 and not 0\n");
-	}
-	else if (lastdigit == 0)
-	{
-	printf(" and is 0\n");
-	}
 
+	lastdigit > 5 ? printf(" and is greater than 5\n") :
+	lastdigit < 6 && lastdigit != 0 ? printf(" and is less than 6 and not 0\n") :
+	lastdigit == 0 ? printf(" and is 0\n") : 0;
+/*
+*	if (lastdigit > 5)
+*	{
+*	printf(" and is greater than 5\n");
+*	}
+*	else if (lastdigit < 6 && lastdigit != 0)
+*	{
+*	printf(" and is less than 6 and not 0\n");
+*	}
+*	else if (lastdigit == 0)
+*	{
+*	printf(" and is 0\n");
+*	}
+*/
 	return (0);
 }
