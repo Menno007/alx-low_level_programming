841bbd177c336ea74335eb49f181a100d4413725
Menno007 <Menna.soft3@gmail.com>
2023-10-21 05:40:54 +0300

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,9 +11,9 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new_node == NULL)
 		return (NULL);
-	new_node->str = strdub(str);
+	new_node->str = strdup(str);
 	new_node->len = strlen(str);
-	new_node->next = head;
-	head = new_node;
+	new_node->next = *head;
+	*head = new_node;
 	return (new_node);
 }
