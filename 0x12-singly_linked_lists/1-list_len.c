a683c2dadb536423695c6a9f7caf4c058d78fd08
Menno007 <Menna.soft3@gmail.com>
2023-10-21 03:47:07 +0300

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_len - counts the number of elements in a linked
+ * @h: pointer to head
+ * Return: the number of elements in a linked
+*/
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	if (h != NULL)
+		return (0);
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
