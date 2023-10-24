bc068b2119f033fc7f32e88a99f7eb344cdb80d1
Menno007 <Menna.soft3@gmail.com>
2023-10-25 01:58:14 +0300

    m

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - function print linked list
+ * @head: pointer to head
+ * @n: f
+ * Return: the number of nodes
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *ptr, *node_end;
+
+	ptr = *head;
+	node_end = malloc(sizeof(listint_t));
+	if (node_end == NULL)
+		return (NULL);
+	node_end->n = n;
+	node_end->next = NULL;
+	if (*head == NULL)
+	{
+		*head = node_end;
+		return (node_end);
+	}
+	while (ptr != NULL)
+		ptr = ptr->next;
+	ptr->next = node_end;
+	return (node_end);
+}
