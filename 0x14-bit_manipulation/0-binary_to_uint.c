65a81654854b32fa2584ab7b14feb241daee3b00
Menno007 <Menna.soft3@gmail.com>
2023-10-27 09:46:32 +0200

    task 0 (convert binary to decimal)

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 /**
  * binary_to_uint - convert binary to decimal
  * @b: string of binary
- * Return: he converted number, or 0
+ * Return: the converted number, or 0
 */
 unsigned int binary_to_uint(const char *b)
 {
@@ -17,8 +17,7 @@ unsigned int binary_to_uint(const char *b)
 		if ((b[i] != '0') && (b[i] != '1'))
 			return (0);
 		/* Multiply 2^i by b[i] and add it to the result */
-		else
-			result += (1 << (n - i - 1)) * (b[i] - '0');
+		result += ((1 << (n - i - 1)) * (b[i] - '0'));
 	}
 	return (result);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 unsigned int binary_to_uint(const char *b);
 #endif
