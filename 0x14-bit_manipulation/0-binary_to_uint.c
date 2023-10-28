5523c81871039b28d7935164132ed56d49755374
Menno007 <Menna.soft3@gmail.com>
2023-10-28 04:35:11 +0200

    prints the binary representation of a number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,3 +21,30 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (result);
 }
+/*
+ *unsigned int binary_to_uint(const char *b)
+ *{
+ *	unsigned int result = 0;
+ *	unsigned int i, n;
+ *
+ *	if (!b)
+ *		return (0);
+ *	n = strlen(b);
+ *	for (i = 0; i < n; i++)
+ *	{
+ *		if ((b[i] != '0') && (b[i] != '1'))
+ *			return (0);
+ *
+ *		--> Multiply 2^i by b[i] and add it to the result <--
+ *
+ *		printf("b[%d]=%c\n\n", i, b[i]);
+ *		printf("n(len)=%d\n", n);
+ *		printf("result before = %d\n", result);
+ *		printf("(n - i - 1)) = %d\n", (n - i - 1));
+ *		printf("(1 << (n - i - 1)) = %d\n", (1 << (n - i - 1)));
+ *		result += ((1 << (n - i - 1)) * (b[i] - '0'));
+ *		printf("result after = %d\n\n", result);
+ *	}
+ *	return (result);
+ *}
+ */
