d043d816c19a661f3f594f11d67babeef1df7048
Menno007 <Menna.soft3@gmail.com>
2023-12-08 01:59:51 +0200

    2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_dnodeint - counts the number of elements
+ * @head: pointer to pointer head
+ * @n: data int
+ * Return: the address of the new element, or NULL if it failed.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	struct dlistint_s *new_node;
+
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->prev = NULL;
+	new_node->next = *head;
+	new_node->n = n;
+	*head = new_node;
+
+	return (new_node);
+}
