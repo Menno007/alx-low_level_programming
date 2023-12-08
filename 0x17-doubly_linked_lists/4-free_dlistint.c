90578bf30402c7f664f91ec3ccccf8fcbc7ecc3a
Menno007 <Menna.soft3@gmail.com>
2023-12-08 03:37:12 +0200

    4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,12 +6,24 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp;
+	if (head == NULL)
+		return;
 
-	while (head != NULL)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
+	if (head->next != NULL)
+		free_dlistint(head->next);
+
+	free(head);
 }
+/*
+* void free_dlistint(dlistint_t *head)
+*{
+*	dlistint_t *temp;
+*
+*	while (head != NULL)
+*	{
+*		temp = head->next;
+*		free(head);
+*		head = temp;
+*	}
+*}
+*/
