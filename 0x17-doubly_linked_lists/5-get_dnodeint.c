81e3af2dcc584f5415bb55af69848ce27d8dea67
Menno007 <Menna.soft3@gmail.com>
2023-12-08 04:44:05 +0200

    5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - frees a doubly linked list
+ * @head: pointer to pointer head
+ * @index: the index of the node
+ * Return: Always 0
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (index == i)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
