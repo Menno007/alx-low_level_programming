9822dc9f9f090bf06839071a0ed3099aad310f6d
Menno007 <Menna.soft3@gmail.com>
2023-12-12 01:04:37 +0200

    8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,19 +9,23 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *ptr = *head;
 
-	while (index--)
-	ptr = ptr->next;
-
-	if (*head == NULL || ptr == NULL || head == NULL)
+	if (*head == NULL)
 		return (-1);
 
-	if (ptr == *head)
+	while (index--)
+		ptr = ptr->next;
+
+	if (ptr == *head) /*idx == 0*/
 	{
-		*head = (*head)->next;
+		*head = (*head)->next; /* move head pointer to next node */
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		free(ptr);
 		return (1);
 	}
-	ptr->next->prev = ptr->prev;
+	if (ptr->next != NULL)
+		ptr->next->prev = ptr->prev;
+
 	ptr->prev->next = ptr->next;
 
 	free(ptr);
